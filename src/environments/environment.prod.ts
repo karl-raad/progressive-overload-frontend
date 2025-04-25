29f5ca977c853cf9f3912927dd9bb43f37b8b079
Kael-at-471112739454 <kael@example.com>
2025-04-25 23:48:47 +0800

    Updated API to v1.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     cognitoUserPoolId: 'ap-southeast-2_yUQbAtMx7',
     cognitoAppClientId: '5dt160orn31kf5emt8mi6jeotv',
-    API_URL: 'https://uo5vaf74da.execute-api.ap-southeast-2.amazonaws.com/prod',
+    API_URL: 'https://eivj7pvjqa.execute-api.ap-southeast-2.amazonaws.com/prod/'
 };
