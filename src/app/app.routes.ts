52beffff41c4fec1e0b3365eeb3e99a393f41053
Kael-at-471112739454 <kael@example.com>
2025-04-25 21:50:18 +0800

    Added maintenance component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,8 +7,10 @@ export const routes: Routes = [
     { path: 'forgot-password', loadComponent: () => import('./auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent) },
     { path: 'confirm-registration/:userEmail', loadComponent: () => import('./auth/confirm-registration/confirm-registration.component').then(m => m.ConfirmRegistrationComponent) },
     { path: 'confirm-password-reset', loadComponent: () => import('./auth/confirm-password-reset/confirm-password-reset.component').then(m => m.ConfirmPasswordResetComponent) },
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    // { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '', redirectTo: '/maintenance', pathMatch: 'full' },
     { path: 'exercise-list', loadComponent: () => import('./exercise/exercise-list/exercise-list.component').then(m => m.ExerciseListComponent), canActivate: [AuthGuard] },
     { path: 'personal-best', loadComponent: () => import('./personal-best/personal-best-list/personal-best-list.component').then(m => m.PersonalBestComponent), canActivate: [AuthGuard] },
-    { path: 'exercise-chart', loadComponent: () => import('./exercise/exercise-chart/exercise-chart.component').then(m => m.ExerciseChartComponent), canActivate: [AuthGuard] }
+    { path: 'exercise-chart', loadComponent: () => import('./exercise/exercise-chart/exercise-chart.component').then(m => m.ExerciseChartComponent), canActivate: [AuthGuard] },
+    { path: 'maintenance', loadComponent: () => import('./maintenance/maintenance.component').then(m => m.MaintenanceComponent) }
 ];
---
+++ src/app/maintenance/maintenance.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-maintenance',
+  standalone: true,
+  imports: [
+    MatIcon
+  ],
+  templateUrl: './maintenance.component.html',
+  styleUrl: './maintenance.component.scss'
+})
+export class MaintenanceComponent {
+
+}
